0313452e2c91bf70f9f18ebcc09878f7d4b8e7b7
Jonathan <wongjonathan.c@gmail.com>
2024-07-27 00:41:35 -0400

    Header now dynamically decides which page we on

--- templates/header-footer.js
+++ templates/header-footer.js
@@ -1,9 +1,10 @@
-// Loading Header into files
+// Loading Header and Footer into files
 document.addEventListener("DOMContentLoaded", function () {
   fetch("templates/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
+      updateActiveLink();
     })
     .catch((error) => console.error("Error loading the header:", error));
 
@@ -13,4 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("footer-container").innerHTML = data;
     })
     .catch((error) => console.error("Error loading the footer:", error));
+
+  // Dynamically change which page on
+  function updateActiveLink() {
+    const currentPath = window.location.pathname;
+
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((link) => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
 });
