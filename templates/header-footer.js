1c0e4584638293f62fb7a1b69847cf04d2691fa1
Jonathan <wongjonathan.c@gmail.com>
2024-07-29 00:52:05 -0400

    header works for subpages, new teams page

--- templates/header-footer.js
+++ templates/header-footer.js
@@ -1,6 +1,6 @@
 // Loading Header and Footer into files
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("templates/header.html")
+  fetch("/templates/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Error loading the header:", error));
 
-  fetch("templates/footer.html")
+  fetch("/templates/footer.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer-container").innerHTML = data;
@@ -17,14 +17,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Dynamically change which page on
   function updateActiveLink() {
-    const currentPath = window.location.pathname;
+    const currentPath = window.location.pathname.replace('.html', '');
 
     const navLinks = document.querySelectorAll(".nav-link");
 
     navLinks.forEach((link) => {
-      if (link.getAttribute("href") === currentPath) {
+      if (currentPath.includes(link.getAttribute("href").replace('.html', ''))) {
         link.classList.add("active");
-      } else {
+      } 
+      else {
         link.classList.remove("active");
       }
     });
