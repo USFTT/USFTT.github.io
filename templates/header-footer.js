3bea442a7185ad820f1b7f986ac48146d235d267
Jonathan Wong <Xmexybutt@gmail.com>
2024-12-18 13:05:55 +0800

    js update

--- templates/header-footer.js
+++ templates/header-footer.js
@@ -1,6 +1,6 @@
 // Loading Header and Footer into files
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../templates/header.html")
+  fetch("header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Error loading the header:", error));
 
-  fetch("../templates/footer.html")
+  fetch("footer.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer-container").innerHTML = data;
